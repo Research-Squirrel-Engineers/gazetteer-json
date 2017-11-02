08d6f2a8ad940760580707d7d4b2bfc26ab7750d
Florian Thiery <web@florian-thiery.de>
2017-11-02 12:43:34 +0100

    add getPlacesByBBox() for Getty TGN

--- src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/GettyTGN.java
+++ src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/GettyTGN.java
@@ -6,13 +6,16 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
-import org.jdom.Element;
+import java.util.Map;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
+import org.linkedgeodesy.gazetteerjson.utils.Functions;
+import org.linkedgeodesy.org.gazetteerjson.json.GGeoJSONFeatureCollection;
+import org.linkedgeodesy.org.gazetteerjson.json.GGeoJSONFeatureObject;
 import org.linkedgeodesy.org.gazetteerjson.json.GGeoJSONSingleFeature;
 import org.linkedgeodesy.org.gazetteerjson.json.NamesJSONObject;
 
@@ -113,6 +116,132 @@ public class GettyTGN {
             }
             json.setGeometry((geometry));
             json.setProperties(placeURI, id, "getty", names);
+        }
+        return json;
+    }
+
+    public static GGeoJSONFeatureCollection getPlacesByBBox(String upperleftLat, String upperleftLon, String upperrightLat, String upperrightLon,
+                                                            String lowerrightLat, String lowerrightLon, String lowerleftLat, String lowerleftLon) throws IOException, ParseException {
+        GGeoJSONFeatureCollection json = new GGeoJSONFeatureCollection();
+        String url = "http://vocab.getty.edu/sparql.json";
+        String queryString = "prefix ontogeo: <http://www.ontotext.com/owlim/geo#> "
+                + "PREFIX geo: <http://www.w3.org/2003/01/geo/wgs84_pos#> "
+                + "PREFIX skos: <http://www.w3.org/2004/02/skos/core#>"
+                + "PREFIX tgn: <http://vocab.getty.edu/tgn/> "
+                + "PREFIX foaf: <http://xmlns.com/foaf/0.1/> "
+                + "PREFIX gvp: <http://vocab.getty.edu/ontology#> "
+                + "PREFIX skosxl: <http://www.w3.org/2008/05/skos-xl#> "
+                + "PREFIX dc: <http://purl.org/dc/elements/1.1/> "
+                + "SELECT DISTINCT ?place ?prefLabel ?altLabel ?lat ?long ?id WHERE { "
+                + "?place skos:inScheme tgn: . "
+                + "?place dc:identifier ?id . "
+                + "?place foaf:focus [ontogeo:within(" + lowerleftLat + " " + lowerleftLon + " " + upperrightLat + " " + upperrightLon + ")]. "
+                + "?place xl:prefLabel [skosxl:literalForm ?prefLabel] . "
+                + "OPTIONAL { ?place xl:altLabel [skosxl:literalForm ?altLabel] }. "
+                + "?place foaf:focus ?p. "
+                + "?p geo:lat ?lat . "
+                + "?p geo:long ?long . "
+                + "}";
+        URL obj = new URL(url);
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+        con.setRequestMethod("POST");
+        String urlParameters = "query=" + queryString;
+        con.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(con.getOutputStream());
+        wr.writeBytes(urlParameters);
+        wr.flush();
+        wr.close();
+        int responseCode = con.getResponseCode();
+        System.out.println("GeoNames.getPlacesByBBox() - " + responseCode + " - " + url);
+        if (responseCode < 400) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
+            JSONObject results = (JSONObject) jsonObject.get("results");
+            JSONArray bindings = (JSONArray) results.get("bindings");
+            // write all binding objects with same "place URI" to hashmap
+            HashMap<String, JSONArray> hm = new HashMap();
+            for (Object item : bindings) {
+                JSONObject binding = (JSONObject) item;
+                JSONObject place = (JSONObject) binding.get("place");
+                String placeValue = (String) place.get("value");
+                if (!hm.containsKey(placeValue)) {
+                    JSONArray tmpArray = new JSONArray();
+                    tmpArray.add(binding);
+                    hm.put(placeValue, tmpArray);
+                } else {
+                    JSONArray tmpArray = hm.get(placeValue);
+                    tmpArray.add(binding);
+                    hm.remove(placeValue);
+                    hm.put(placeValue, tmpArray);
+                }
+            }
+            // loop through hashmap
+            for (Map.Entry<String, JSONArray> entry : hm.entrySet()) {
+                String key = entry.getKey();
+                //System.out.println(key);
+                JSONArray value = (JSONArray) entry.getValue();
+                //System.out.println(value.size());
+                JSONObject val0 = (JSONObject) value.get(0);
+                // add and get names
+                NamesJSONObject names = new NamesJSONObject();
+                // get prefLabels
+                JSONObject prefLabelObj = (JSONObject) val0.get("prefLabel");
+                String prefLabelString = (String) prefLabelObj.get("value");
+                String prefLabelLang = (String) prefLabelObj.get("xml:lang");
+                if (prefLabelLang == null) {
+                    prefLabelLang = "en";
+                }
+                HashSet hs = new HashSet();
+                hs.add(prefLabelString);
+                names.setName(prefLabelLang, hs);
+                // get id
+                JSONObject idObj = (JSONObject) val0.get("id");
+                String id = (String) idObj.get("value");
+                // add and get geometry
+                JSONObject lat = (JSONObject) val0.get("lat");
+                JSONObject lng = (JSONObject) val0.get("long");
+                GGeoJSONFeatureObject feature = new GGeoJSONFeatureObject();
+                JSONArray point = new JSONArray();
+                point.add(Double.parseDouble((String) lng.get("value")));
+                point.add(Double.parseDouble((String) lat.get("value")));
+                JSONObject geometry = new JSONObject();
+                geometry.put("type", "Point");
+                geometry.put("coordinates", point);
+                feature.setGeometry(geometry);
+                feature.setProperties(key, id, "getty", names);
+                json.setFeature(feature);
+                // get distance
+                JSONArray bbox = Functions.bboxCenter(Double.parseDouble(lowerrightLon), Double.parseDouble(upperleftLon), Double.parseDouble(upperleftLat), Double.parseDouble(upperrightLat));
+                Double bboxlon = (Double) bbox.get(1);
+                Double bboxlat = (Double) bbox.get(0);
+                feature.setPropertiesDistanceSimilarity(bboxlon, bboxlat, Double.parseDouble((String) lat.get("value")), Double.parseDouble((String) lng.get("value")));
+                for (Object item : value) {
+                    JSONObject val = (JSONObject) item;
+                    // get altLabels
+                    JSONObject altLabelObj = (JSONObject) val.get("altLabel");
+                    if (altLabelObj != null) {
+                        String altLabelString = (String) altLabelObj.get("value");
+                        String altLabelLang = (String) altLabelObj.get("xml:lang");
+                        // add altLabel to names array
+                        if (altLabelLang == null) {
+                            altLabelLang = "en";
+                        }
+                        if (names.getNamesByLanguage(altLabelLang) != null) {
+                            names.addName(altLabelLang, altLabelString);
+                        } else {
+                            hs.add(altLabelString);
+                            names.setName(altLabelLang, hs);
+                        }
+                    }
+                }
+            }
+            json.setMetadata("getty", upperleftLat, upperleftLon, upperrightLat, upperrightLon, lowerrightLat, lowerrightLon, lowerleftLat, lowerleftLon, null);
         }
         return json;
     }
