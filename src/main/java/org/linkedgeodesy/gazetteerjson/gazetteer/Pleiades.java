bcfe60330e3521e69b399194c3cfbd5deb3e82dc
Florian Thiery <web@florian-thiery.de>
2017-11-02 14:33:17 +0100

    add getPlaceById() for Pleiades via Peripleo

--- src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/Pleiades.java
+++ src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/Pleiades.java
@@ -1,76 +1,65 @@
-/*
- GET url with accept: application/vnd.geo+json 
-
-add pleiades search in bounding box lookup and cendroid search
-
-is periopleo api the right way to do it?
-
-https://github.com/pelagios/peripleo/blob/master/README.md#bbox
-
-http://pelagios.org/peripleo/search?bbox=23.716,23.7266,37.97,37.978&types=place
-
-https://github.com/pelagios/peripleo/blob/master/README.md#lat-lon-radius
-
-http://pelagios.org/peripleo/search?lat=37.97&lon=23.72&radius=3&types=place
-
-----------------
-
-<Is this the right way to use it? Because the Pelagios API is tagged with 
-“deprecated”.>
-
-Yes, that should work. Be sure to check not just the "identifier" field, 
-though, but also the "matches" field, if it exists. I think in practice, the 
-records will pretty much always have the Pleiades URI in the "identifier", 
-and only the alternative gazetteers (DARE, Vici, Trismegistos, etc.) in the 
-"matches" - but it's not guaranteed!
-
-With the deprecation warning, I assume you are referring to the note on the 
-GitHub project? Yes, that's because I'm currently working on the new version 
-at
-
-http://github.com/pelagios/peripleo2
-
-It will still take a while until Peripleo 2 replaces the current one though. 
-Also, I'm planning a backwards compatible API crosswalk, so that existing 
-clients will (hopefully) notice no difference. (But you really won't want to 
-use that, since the new API will be so much cooler 
-
-<I also looking for the lat, lon, radius function >
-
-Yes, that should work fine, too.
-
-Let me know if you build something interesting! We're constantly on the 
-lookout for people making use of our stuff in their own work.
-
-Cheers,
-Rainer
-
-----------------
-
-Hi @ all,
-
-one simple question: I try to query Pleiades Places within a Bounding Box or 
-via Lat, Lon and Radius.
-
-I thought in this case the Peripleo API could solve the problem.
-
-If I use e.g. 
-http://pelagios.org/peripleo/search?bbox=23.716,23.7266,37.97,37.978&types=place 
-and filter the results where “identifier” contains “plaiades” I could get 
-some Pleiades places.
-
-Is this the right way to use it? Because the Pelagios API is tagged with 
-“deprecated”.
-
-I also looking for the lat, lon, radius function 
-(https://github.com/pelagios/peripleo/blob/master/README.md#lat-lon-radius).
-
-Best Regards from Mainz,
-
-
- */
 package org.linkedgeodesy.gazetteerjson.gazetteer;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashSet;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.linkedgeodesy.org.gazetteerjson.json.GGeoJSONSingleFeature;
+import org.linkedgeodesy.org.gazetteerjson.json.NamesJSONObject;
+
+/**
+ * functions to query Pleiades Places
+ *
+ * @author Florian Thiery
+ */
 public class Pleiades {
-    
+
+    public static GGeoJSONSingleFeature getPlaceById(String id) throws IOException, ParseException {
+        GGeoJSONSingleFeature json = new GGeoJSONSingleFeature();
+        String uri = "http://pelagios.org/peripleo/places/http:%2F%2Fpleiades.stoa.org%2Fplaces%2F" + id;
+        URL url = new URL(uri);
+        HttpURLConnection con = (HttpURLConnection) url.openConnection();
+        con.setRequestMethod("GET");
+        int responseCode = con.getResponseCode();
+        System.out.println("Pleiades.getPlaceById() - " + responseCode + " - " + url);
+        if (responseCode < 400) {
+            BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream(), "UTF-8"));
+            String inputLine;
+            StringBuffer response = new StringBuffer();
+            while ((inputLine = in.readLine()) != null) {
+                response.append(inputLine);
+            }
+            in.close();
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(response.toString());
+            // get geom
+            JSONObject geo_bounds = (JSONObject) jsonObject.get("geo_bounds");
+            JSONObject geometry = new JSONObject();
+            JSONArray point = new JSONArray();
+            point.add((Double) geo_bounds.get("min_lon"));
+            point.add((Double) geo_bounds.get("min_lat"));
+            geometry.put("type", "Point");
+            geometry.put("coordinates", point);
+            // get and add prefName
+            NamesJSONObject names = new NamesJSONObject();
+            String prefName = (String) jsonObject.get("title");
+            names.addPrefName("unknown", prefName);
+            // get and add alternative names
+            JSONArray namesArray = (JSONArray) jsonObject.get("names");
+            names.setName("unknown", new HashSet());
+            for (Object item : namesArray) {
+                String tmp = (String) item;
+                names.addName("unknown", tmp);
+            }
+            json.setGeometry(geometry);
+            json.setProperties("https://pleiades.stoa.org/places/" + id, id, "pleiades", names);
+        }
+        return json;
+    }
+
 }
