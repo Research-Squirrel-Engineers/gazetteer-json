1f692725c6c0152ac3a595558fe0540ff5ab6a32
Florian Thiery <web@florian-thiery.de>
2017-11-03 10:56:08 +0100

    remove unused import

--- src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/GeoNames.java
+++ src/main/java/org/linkedgeodesy/gazetteerjson/gazetteer/GeoNames.java
@@ -7,7 +7,6 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.HashSet;
 import java.util.List;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
